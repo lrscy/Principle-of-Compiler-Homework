3ae1fccd2c641636b5430ac2f946d61c4f4259a0
lrscy <ruosenlee@gmail.com>
2016-04-28 20:37:54 +0800

    1. 增加代码注释 2. 增加测试用例
    Signed-off-by: lrscy <ruosenlee@gmail.com>

--- 专题1_词法分析程序设计与实现/Lexer.cpp
+++ 专题1_词法分析程序设计与实现/Lexer.cpp
@@ -1,11 +1,29 @@
 #include "lexer.h"
 
+/*
+ * 功能：
+ *  判断当前阶段是否为终结阶段
+ * 传入参数：
+ *  line:包含该行的字符串
+ * 传出参数：（无）
+ * 返回值：
+ *  该阶段是否成功解析。是则返回true，否则返回false。
+ */
 bool isTerminalStage( int stage ) {
     if( stage == 0 || stage == 4 || stage == 5 ||
         stage == 11 || stage == 12 || stage == 14 ) return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  判断当前字符的类型
+ * 传入参数： 
+ *  ch:待判断字符
+ * 传出参数：（无）
+ * 返回值：
+ *  该字符的类型对应的值。
+ */
 int characterType( char ch ) {
     if( ch == '\t' || ch == ' ' || ch == '\n' || ch == 0 ) return 0;
     switch( ch ) {
@@ -35,6 +53,15 @@ int characterType( char ch ) {
     return -1;
 }
 
+/*
+ * 功能： 
+ *  将double类型值转换为二进制表示。
+ * 传入参数： 
+ *  d:待转换的double值
+ * 传出参数：
+ *  str:该double类型值得二进制表示字符串
+ * 返回值：（无）
+ */
 void dtob( double d, string &str ) {
     int a, cnt = 0, flag = 0;
     if( d < 0 ) { d = -d; flag = 1; }
@@ -57,6 +84,15 @@ void dtob( double d, string &str ) {
     return ;
 }
 
+/*
+ * 功能： 
+ *  将得到的合法字符串加入到二元式序列中
+ * 传入参数： 
+ *  str:待加入二元式的合法字符串
+ * 传出参数：
+ *  vTable:更新后的二元式序列
+ * 返回值：（无）
+ */
 void addPair( string str, vector<PIS> &vTable ) {
     if( str == "+" )       vTable.push_back( make_pair( 1, "-" ) );
     else if( str == "-" )  vTable.push_back( make_pair( 2, "-" ) );
@@ -113,6 +149,17 @@ void addPair( string str, vector<PIS> &vTable ) {
     return ;
 }
 
+/*
+ * 功能： 
+ *  阶段0处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_0( int &stage, int chtype ) {
     if( chtype == 0 ) stage = 0;
     else if( ( 3 <= chtype && chtype <= 5 ) ||
@@ -131,54 +178,153 @@ bool stage_0( int &stage, int chtype ) {
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段1处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_1( int &stage, int chtype ) {
     if( chtype == 8 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段2处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_2( int &stage, int chtype ) {
     if( chtype == 6 || chtype == 8 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段3处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_3( int &stage, int chtype ) {
     if( chtype == 7 || chtype == 8 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段4处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_4( int &stage, int chtype ) {
     if( chtype == 10 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段5处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_5( int &stage, int chtype ) {
     if( chtype == 11 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段6处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_6( int &stage, int chtype ) {
     if( 19 <= chtype && chtype <= 80 ) stage = 6;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段7处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_7( int &stage, int chtype ) {
     if( chtype == 1 || chtype == 8 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段8处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_8( int &stage, int chtype ) {
     if( chtype == 2 || chtype == 8 ) stage = 15;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段9处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_9( int &stage, int chtype ) {
     if( chtype == 33 || chtype == 59 ) stage = 11;
     else if( chtype == 18 ) stage = 10;
@@ -187,6 +333,17 @@ bool stage_9( int &stage, int chtype ) {
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段10处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_10( int &stage, int chtype ) {
     if( chtype == 33 || chtype == 59 ) stage = 11;
     else if( 19 <= chtype && chtype <= 28 ) stage = 10;
@@ -194,6 +351,17 @@ bool stage_10( int &stage, int chtype ) {
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段11处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_11( int &stage, int chtype ) {
     if( 19 <= chtype && chtype <= 28 ) stage = 13;
     else if( chtype == 1 || chtype == 2 ) stage = 12;
@@ -201,28 +369,83 @@ bool stage_11( int &stage, int chtype ) {
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段12处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_12( int &stage, int chtype ) {
     if( 19 <= chtype && chtype <= 28 ) stage = 13;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段13处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_13( int &stage, int chtype ) {
     if( 19 <= chtype && chtype <= 28 ) stage = 13;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段14处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_14( int &stage, int chtype ) {
     if( 19 <= chtype && chtype <= 28 ) stage = 10;
     else return false;
     return true;
 }
 
+/*
+ * 功能： 
+ *  阶段15处理
+ * 传入参数： 
+ *  stage:当前阶段
+ *  chtype:下一个字符的类别
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool stage_15( int &stage, int chtype ) {
     return false;
 }
 
+/*
+ * 功能： 
+ *  阶段转换
+ * 传入参数： 
+ *  Stage:当前阶段
+ *  ch:下一个字符
+ * 传出参数：
+ *  stage:下一个阶段
+ * 返回值：
+ *  是否进入下一个合法的阶段。进入返回true，否则返回false。
+ */
 bool excute( int &stage, char ch ) {
     int chtype = characterType( ch );
     bool flag = true;
@@ -248,6 +471,19 @@ bool excute( int &stage, char ch ) {
     return flag;
 }
 
+/*
+ * 功能： 
+ *  行词法分析
+ * 传入参数： 
+ *  line:包含该行的字符串
+ *  colNo:起始处理位置
+ * 传出参数：
+ *  colNo:处理完成位置
+ *  errmsg:错误信息
+ *  vTable:该行的二元式序列
+ * 返回值：
+ *  该行是否成功解析。成功返回true，否则返回false。
+ */
 bool lineAnalyse( string &line, int &colNo, string &errmsg, vector<PIS> &vTable ) {
     int stage = 0, st = 0, prestage = 0, len = line.length();
     bool flag = true;
--- 专题1_词法分析程序设计与实现/main.cpp
+++ 专题1_词法分析程序设计与实现/main.cpp
@@ -1,5 +1,12 @@
 #include "lexer.h"
 
+/*
+ * 功能：
+ *  得到输出文件名
+ * 传入参数：inputname:需处理的文件的文件名
+ * 传出参数：outputname:输出文件的文件名
+ * 返回值：（无）
+ */
 void getOutputName( char *inputName, char *outputName ) {
     int len = strlen( inputName );
     strcpy( outputName, inputName );
@@ -10,11 +17,34 @@ void getOutputName( char *inputName, char *outputName ) {
     return ;
 }
 
+/*
+ * 功能：
+ *  向屏幕输出错误信息
+ * 传入参数： 
+ *  filename:正在处理的文件的文件名称
+ *  rowNo:出错行
+ *  colNo:出错列
+ *  errmsg:错误信息
+ * 传出参数：（无）
+ * 返回值：（无）
+ */
 void errMsg( string filename, int rowNo, int colNo, string errmsg ) {
     printf( "%s:%d:%d error: %s\n", filename.c_str(), rowNo, colNo, errmsg.c_str() );
     return ;
 }
 
+/*
+ * 功能：
+ *  预处理，删除字符串内注释
+ * 传入参数： 
+ *  str:需处理的字符串
+ *  Noteflag:该字符串是否处于多行注释中
+ * 传出参数：
+ *  str:处理过的字符串
+ *  noteflag:当前行是否处于多行注释中
+ * 返回值：
+ *  是否成功处理。如果注释匹配则返回true，否则返回false。
+ */
 bool noteProcess( string &str, bool &noteflag, string &errmsg ) {
     while( true ) {
         int p1 = str.find( "//" );
@@ -47,6 +77,15 @@ bool noteProcess( string &str, bool &noteflag, string &errmsg ) {
     return true;
 }
 
+/*
+ * 功能：
+ *  将结果输出到文件中
+ * 传入参数： 
+ *  fp:输出文件指针
+ *  vTable:二元式序列
+ * 传出参数：（无）
+ * 返回值：（无）
+ */
 void print( FILE *fp, vector<PIS> &vTable ) {
     for( int i = 0; i < vTable.size(); ++i ) {
         fprintf( fp, "( %d, %s )\n", vTable[i].first, vTable[i].second.c_str() );
@@ -62,6 +101,7 @@ int main( int argc, char **argv ) {
     string errmsg;
     vector<PIS> vTable;
     
+    // 文件后缀处理
     getOutputName( argv[1], outputName );
     finp = fopen( argv[1], "r" );
     foutp = fopen( outputName, "w" );
@@ -69,6 +109,7 @@ int main( int argc, char **argv ) {
     foutp = fopen( outputName, "a+" );
     getOutputName( argv[1], outputName );
 
+    // 行处理
     rowNo = 0;
     while( NULL != fgets( line, MAXLEN, finp ) ) {
         vTable.clear();
@@ -77,6 +118,7 @@ int main( int argc, char **argv ) {
         colNo = 0;
         string str = line;
         str = str.substr( 0, LINEMAXLEN );
+        // 注释预处理及词法分析
         if( !noteProcess( str, noteflag, errmsg ) ||
                 ( str.length() > 0 && !lineAnalyse( str, colNo, errmsg, vTable ) ) ) {
             flag = false;
